5c2421c5dfb27f161e2efc0207cc7e5f63029c65
kiciarozek <73694473+kiciarozek@users.noreply.github.com>
2021-12-19 21:02:26 +0100

    More features
    More features

--- symulacja-upadku.py
+++ symulacja-upadku.py
@@ -2,83 +2,161 @@ import pygame
 from math import sqrt
 pygame.init()
 
-# Set up the drawing window
-screen = pygame.display.set_mode([500, 950])
+screen = pygame.display.set_mode([1000, 950])
 
-# ball position
+display_surface = pygame.display.set_mode((1000,950))
+
 x = 250
 y = 875
-# starting height
+
 h0 = y
-# max height
 hmax = h0
-# when to stop bouncing
 hstop = 0.1
 
-# Is ball in air?
 freefall = True
 
-# bounciness
 rho = 0.75
 t = 1
 tau = 0.1
 t_last = 0
-# speed of simulation
-dt = 0.025
-# gravitation {earth = 9.81}
+dt = 0.05
 g = 9.81
-# velocity
 v = 0
 
 vmax = sqrt(2 * hmax * g)
 
-# Run until the user asks to quit
+try:
+    font = pygame.font.Font('RobotoMono-Regular.ttf', 32)
+except:
+    font = pygame.font.Font(None,40)
+
+ziemia = font.render('Earth', True,(0,0,0),(0,150,0) )
+ziemiaRect = ziemia.get_rect()
+ziemiaRect.center = (1000-100,30)
+
+mars = font.render('Mars', True,(200,200,200),(0,0,0) )
+marsRect = mars.get_rect()
+marsRect.center = (1000-100,90)
+
+ksiezyc = font.render('Moon', True,(200,200,200),(0,0,0) )
+ksiezycRect = ksiezyc.get_rect()
+ksiezycRect.center = (1000-100,150)
+
+inputtext = "1"
+
+text = font.render("Speed:", True,(200,200,200),(0,0,0) )
+textRect = text.get_rect()
+textRect.center = (1000-100,210)
+
+inputbox = font.render(inputtext, True,(200,200,200),(0,0,0) )
+inputboxRect = inputbox.get_rect()
+inputboxRect.center = (1000-100,270)
+
+inputactive = False
+
 running = True
 while running:
-    # Did the user click the window close button?
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+
     # if bouncing is taking too long end it
     if hmax > hstop:
         # is ball in air?
         if freefall:
             # set the new height based off some science stuff
             hnew = y + v * dt - 0.5 * g * dt * dt
-            # is ball touching ground
+            # is ball touching ground, if not stop it
             if y < 0:
-                # do some calculations and set freefall as False
                 t = t_last + 2 * sqrt(2 * hmax / g)
                 freefall = False
                 t_last = t + tau
-                # make sure ball is on ground
                 y = 0
             # if it is in air
             else:
                 t = t + dt
-                # make velocity higher based off gravitation and time
                 v = v - g * dt
                 y = hnew
         # if it is touching ground
         else:
-            # do some science stuff, boooring
             t = t + tau
             vmax = vmax * rho
             v = vmax
             freefall = True
             y = 0
-        # make sure ball won't reach too high
         hmax = 0.5 * vmax * vmax / g
 
 
-    # Fill the background with white
     screen.fill((255, 255, 255))
 
-    # draw a circle
-    pygame.draw.circle(screen, (0, 0, 255), (x, 875 - y), 75)
+    circle = pygame.draw.circle(screen, (0, 0, 255), (x, 875 - y), 75)
 
-    # Flip the display
+    display_surface.blit(ziemia, ziemiaRect)
+    display_surface.blit(mars, marsRect)
+    display_surface.blit(ksiezyc, ksiezycRect)
+
+    display_surface.blit(inputbox, inputboxRect)
+    display_surface.blit(text, textRect)
+
+    inputbox = font.render(inputtext, True, (200, 200, 200), (0, 0, 0))
+
+    if pygame.mouse.get_pressed()[0]:
+        if circle.collidepoint(pygame.mouse.get_pos()):
+            hmax = y
+            mx , my = pygame.mouse.get_pos()
+            y = 875 - my
+            x = mx
+            hstop = 0.1
+            freefall = True
+            rho = 0.75
+            t = 1
+            tau = 0.1
+            t_last = 0
+            v = 0
+            if hmax > 0:
+                vmax = sqrt(2 * hmax * g)
+            else:
+                vmax = 0
+
+        elif ziemiaRect.collidepoint(pygame.mouse.get_pos()):
+            ziemia = font.render('Ziemia', True, (0, 0, 0), (0, 150, 0))
+            mars = font.render('Mars', True, (200, 200, 200), (0, 0, 0))
+            ksiezyc = font.render('Kśiężyc', True, (200, 200, 200), (0, 0, 0))
+            g = 9.81
+            freefall = False
+
+        elif marsRect.collidepoint(pygame.mouse.get_pos()):
+            ziemia = font.render('Ziemia', True, (200, 200, 200), (0, 0, 0))
+            mars = font.render('Mars', True, (0, 0, 0), (0, 150, 0))
+            ksiezyc = font.render('Kśiężyc', True, (200, 200, 200), (0, 0, 0))
+            g = 3.72
+            freefall = False
+
+        elif ksiezycRect.collidepoint(pygame.mouse.get_pos()):
+            ziemia = font.render('Ziemia', True, (200, 200, 200), (0, 0, 0))
+            mars = font.render('Mars', True, (200, 200, 200), (0, 0, 0))
+            ksiezyc = font.render('Kśiężyc', True, (0, 0, 0), (0, 150, 0))
+            g = 1.62
+            freefall = False
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if inputboxRect.collidepoint(pygame.mouse.get_pos()):
+                inputactive = True
+                inputtext = ""
+        elif event.type == pygame.KEYDOWN and inputactive:
+
+            if event.key == pygame.K_RETURN:
+                inputactive = False
+                dt = float(inputtext) * 0.05
+            elif event.key == pygame.K_BACKSPACE:
+                inputtext = inputtext[:-1]
+            else:
+                inputtext += event.unicode
+
+    if inputactive:
+        inputbox = font.render(inputtext, True, (0, 0, 0), (0, 150, 0))
+
     pygame.display.flip()
 
-# Done! Time to quit.
 pygame.quit()
